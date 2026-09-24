341d942fb6d54e4d2f044471679342a4af51bc73
Oluwaseun-30 <Utility12345678910@gmail.com>
2026-09-23 18:29:26 -0600

    Blue

--- app/src/main/java/com/example/teamsolace/Shape.kt
+++ app/src/main/java/com/example/teamsolace/Shape.kt
@@ -2,6 +2,7 @@ package com.example.teamsolace
 
 abstract class Shape (
     val X: Int,
-    val y: Int,
-    val color: String = "blue"
-) {}
+    val y: Int
+) {
+    val colour: String = "Blue"
+}
